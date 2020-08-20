bc30ccb0ffaa37bdbaa193bf7f51d838047f1bf3
Eldrick Wega <eldrickman19@gmail.com>
2020-08-20 14:48:44 +0000

    Update for Cloud Function (#2)
    This update made a couple changes for both the local use and to start migration towards Cloud Functions.
    
    1) Added folder to host data on Video Intelligence API extraction in the form of CSV
    
    2) Made changes to personTrackingExtractor and main scripts to enable pulling from an online storage

--- main.py
+++ main.py
@@ -9,27 +9,46 @@ from infrastructure import tools
 import pandas as pd
 import sys
 
-# Define important variables
-video_name = sys.argv[1]
-# Getting video fies locally
-video_path = ['videos/input/', video_name + '.mp4']
-output_path = ['videos/output/', video_name + '.avi']
+def social_distancing_detection(video_name, storage_online):
+    # Define important variables
+    if storage_online: # Get video files from Cloud Storage
+        print('You decided to pull from from Cloud Storage.')
+        video_path = ['gs://test-bucket-1997/input/', video_name + '.mp4']
+        output_path = ['videos/output/', video_name + '.avi']
+        data_path = ['videos/data/', video_name + '.csv']
+    else: # Get video files locally
+        print('File taken from local "videos/input/" folder.')
+        video_path = ['videos/input/', video_name + '.mp4']
+        output_path = ['videos/output/', video_name + '.avi']
+        data_path = ['videos/data/', video_name + '.csv']
 
-# Extract person tracking data
-person_tracking_array = personTrackingExtractor2.detect_person(video_path)
-detection_df = pd.DataFrame(person_tracking_array, columns=['start_s', 'end_s', 'left', 'top', 'right', 'bottom'])
-print(detection_df)
 
-# Perform light data manipulation
-detection_df.sort_values(by=['start_s']) # Sort by Start time
-fps = get_fps(video_path)
-print(fps)
-detection_df['start_s'] = detection_df['start_s'].multiply(fps) # Convert to frames
-detection_df['end_s'] = detection_df['end_s'].multiply(fps) # Convert to frames 
-detection_df.rename(columns={"start_s": "start_f", "end_s": "end_f"}, inplace=True) # Rename columns to describe fps
-print(detection_df)
+    # Extract person tracking data
+    person_tracking_array = personTrackingExtractor2.detect_person(video_path, storage_online)
+    detection_df = pd.DataFrame(person_tracking_array, columns=['start_s', 'end_s', 'left', 'top', 'right', 'bottom'])
+    print(detection_df)
 
-x = input('Proceed?')
+    # Exporting dataframe to CSV
+    detection_df.to_csv(path_or_buf=''.join(data_path),index=False)
 
-# Display person tracking
-draw_detections(video_path, output_path, detection_df, fps)
+    # Perform light data manipulation
+    detection_df.sort_values(by=['start_s']) # Sort by Start time
+    fps = get_fps(video_path)
+    print(fps)
+    detection_df['start_s'] = detection_df['start_s'].multiply(fps) # Convert to frames
+    detection_df['end_s'] = detection_df['end_s'].multiply(fps) # Convert to frames 
+    detection_df.rename(columns={"start_s": "start_f", "end_s": "end_f"}, inplace=True) # Rename columns to describe fps
+    print(detection_df)
+
+    # Display person tracking
+    draw_detections(video_path, output_path, detection_df, fps)
+
+# Call the funtion
+if len(sys.argv) > 1:
+    video_name = sys.argv[1]
+    storage_online = False
+else:
+    video_name = 'one-by-one-person-detection'
+    storage_online = True
+
+social_distancing_detection(video_name, storage_online)

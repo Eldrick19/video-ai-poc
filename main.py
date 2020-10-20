e6d22e572e31df5d1b76594f8f345b5d3dc00c2f
Eldrick Wega <eldrickman19@gmail.com>
2020-10-20 15:06:31 +0000

    Opencv hog implementation (#9)
    * OpenCV Reader
    
    * Implement OpenCV vision
    
    Adds a 3rd vision extractor (this time not using the Google API but using OpenCV). HOG

--- main.py
+++ main.py
@@ -4,11 +4,10 @@
 
 # Import Libraries
 from google.cloud import videointelligence_v1p3beta1 as videointelligence
-from extraction import personTrackingExtractor, personTrackingExtractor2, frameBoxesExtractor
+from extraction import personTrackingExtractor, personTrackingExtractor2, personTrackingExtractor3, frameBoxesExtractor
 from algorithms import socialDistancingTagger
 from data import bqJobHelper
 from frontend import outputVisualizer
-from infrastructure import tools
 from pathlib import Path
 import pandas as pd
 import openpyxl
@@ -30,54 +29,40 @@ def social_distancing_detection(video_name, storage_online, call, blur_faces):
         output_path = ['videos/output/', video_name + '.avi']
         data_path = ['videos/data/', video_name + '.xlsx']
 
-    if not Path(''.join(data_path)).is_file():
-        # EXTRACTION - Extract person tracking data using Google API
-        print('\nCalling Video Intelligence API with feature '+call+'...')
-        start_time = time.time()
-        if call == 'PERSON_DETECTION':
-            person_tracking_array = personTrackingExtractor.detect_person(video_path, storage_online)
-        elif call == 'OBJECT_TRACKING':
-            person_tracking_array = personTrackingExtractor2.detect_person(video_path, storage_online)
-        detection_df = pd.DataFrame(person_tracking_array, columns=['d_id','start_s', 'left', 'top', 'right', 'bottom'])
-        detection_df.to_excel(''.join(data_path),sheet_name='0')  # Exporting dataframe to CSV
-        # Save runtime data
-        if storage_online == False:
-            api_runtime = time.time() - start_time
-            runtime_df = pd.DataFrame(columns=['function', 'runtime_s'])
-            runtime_df = runtime_df.append({'function': 'api_call_'+call, 'runtime_s': api_runtime}, ignore_index=True)              
-    else:
-        detection_df = pd.read_excel(''.join(data_path),sheet_name='0')
-        runtime_df = pd.read_excel(''.join(data_path),sheet_name='1')
-        if 'api_call_'+call not in list(runtime_df['function']): # EXTRACTION - Data has already been extracted but not using the new call
-            print('\nData already extracted for this video, but not using the '+call+' call. Calling Video Intelligence API with feature '+call+'...')
-            start_time = time.time()
-            if call == 'PERSON_DETECTION':
-                person_tracking_array = personTrackingExtractor.detect_person(video_path, storage_online)
-            elif call == 'OBJECT_TRACKING':
-                person_tracking_array = personTrackingExtractor2.detect_person(video_path, storage_online)
-            detection_df = pd.read_excel(''.join(data_path),sheet_name='0')
-            if storage_online == False:
-                api_runtime = time.time() - start_time
-                runtime_df = pd.read_excel(''.join(data_path),sheet_name='1')
-                runtime_df = runtime_df.append({'function': 'api_call_'+call, 'runtime_s': api_runtime}, ignore_index=True) 
-        else: # EXTRACTION - If it has already been extracted, just get from saved data
-            print('\nData already extracted for this video using the '+call+' call. No need to call API.')
+    fps, frame_count, video_dimensions = frameBoxesExtractor.get_video_data(video_path)
+  
+    # EXTRACTION - Extract person tracking data using Google API
+    print('\nCalling Video Intelligence API with feature '+call+'...')
+    start_time = time.time()
+    if call == 'PERSON_DETECTION':
+        person_tracking_array = personTrackingExtractor.detect_person(video_path, storage_online)
+    elif call == 'OBJECT_TRACKING':
+        person_tracking_array = personTrackingExtractor2.detect_person(video_path, storage_online)
+    elif call == 'HOG_OPENCV':
+        person_tracking_array = personTrackingExtractor3.detect_person(video_path, fps, video_dimensions)
+    detection_df = pd.DataFrame(person_tracking_array, columns=['d_id','start_s', 'left', 'top', 'right', 'bottom'])
+    detection_df.to_excel(''.join(data_path),sheet_name='0')  # Exporting dataframe to CSV
+    # Save runtime data
+    if storage_online == False:
+        api_runtime = time.time() - start_time
+        runtime_df = pd.DataFrame(columns=['function', 'runtime_s'])
+        runtime_df = runtime_df.append({'function': 'api_call_'+call, 'runtime_s': api_runtime}, ignore_index=True)              
 
     # EXTRACTION - Perform light data manipulation
     start_time = time.time()
     detection_df = detection_df.sort_values(by=['start_s']) # Sort by Start time
     detection_df = detection_df.reset_index(drop=True)
-    fps, frame_count, video_dimensions = frameBoxesExtractor.get_video_data(video_path)
     detection_df['start_s'] = detection_df['start_s'].multiply(fps) # Convert to frames
     detection_df.rename(columns={"start_s": "start_f"}, inplace=True) # Rename columns to describe fps
 
     # EXTRACTION - Perform heavier data manipulation. Gets all bounding boxes to be displayed at each frame
     print('\nHeavy data manipulation (bounding box at each frame)...')
-    frame_detections = frameBoxesExtractor.detections_at_each_frame(detection_df, frame_count, video_dimensions)
+    frame_detections = frameBoxesExtractor.detections_at_each_frame(detection_df, frame_count, video_dimensions, fps)
     if storage_online == False:
         data_runtime = time.time() - start_time
         runtime_df = runtime_df.append({'function': 'data_manipulation', 'runtime_s': data_runtime}, ignore_index=True)
-    detection_interval = frameBoxesExtractor.detection_frame_interval(frame_detections)
+
+    detection_interval = frameBoxesExtractor.detection_frame_interval(frame_detections, call)
     
     # ALGORITHMS - Highlight all instances of non social distancing
     print('\nRunning social distanding algorithms...')
@@ -125,4 +110,4 @@ else:
     storage_online = True
 
 
-social_distancing_detection(video_name, storage_online, 'PERSON_DETECTION', blur_faces=True)
+social_distancing_detection(video_name, storage_online, 'HOG_OPENCV', blur_faces=True)
